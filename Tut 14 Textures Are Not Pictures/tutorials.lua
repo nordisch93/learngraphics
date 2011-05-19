bf43d0a775ec148b431127ed268cee6b8ffea518
Alfonse <Alfonse@localhost>
2011-05-19 00:22:04 -0700

    Changed the transform pipeline diagram.

---
+++ Tut 14 Textures Are Not Pictures/tutorials.lua
@@ -0,0 +1,10 @@
+SetupProject("Tut 14 Basic Texture", "Basic Texture.cpp",
+	"data/PN.vert",
+	"data/ShaderGaussian.frag",
+	"data/TextureGaussian.frag")
+
+SetupProject("Tut 14 Perspective Interpolation", "Perspective Interpolation.cpp",
+	"data/SmoothVertexColors.vert",
+	"data/SmoothVertexColors.frag",
+	"data/NoCorrectVertexColors.vert",
+	"data/NoCorrectVertexColors.frag")
