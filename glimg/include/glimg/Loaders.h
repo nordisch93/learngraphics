5b6bb5b54f677e843c3fbffc37e4cf22b5b99130
alfonse <alfonse@localhost>
2011-08-01 23:12:09 -0700

    New Collada Converter application and new converted meshes that load faster.

---
+++ glimg/include/glimg/Loaders.h
@@ -0,0 +1,14 @@
+#ifndef GLIMG_LOADERS_H
+#define GLIMG_LOADERS_H
+
+/**
+\file
+
+\brief Includes all of the image loaders.
+**/
+
+#include "StbLoader.h"
+#include "DdsLoader.h"
+
+
+#endif //GLIMG_LOADERS_H
