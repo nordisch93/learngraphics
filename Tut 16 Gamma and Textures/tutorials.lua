e39636188ae8bc59866637029cb1668207d12141
Alfonse <Alfonse@localhost>
2011-10-27 22:19:11 -0700

    Updated to GL SDK 0.4.0.

---
+++ Tut 16 Gamma and Textures/tutorials.lua
@@ -0,0 +1,8 @@
+
+SetupProject("Tut 16 Gamma Ramp", "GammaRamp.cpp",
+	"data/screenCoords.vert",
+	"data/textureGamma.frag", "data/textureNoGamma.frag")
+
+SetupProject("Tut 16 Gamma Checkers", "Gamma Checkers.cpp",
+	"data/PT.vert",
+	"data/textureGamma.frag", "data/textureNoGamma.frag")
